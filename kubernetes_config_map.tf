83a105e51cc2c6d85c2ccf1fb91767f88b31e498
danielrdeleo <danielrdeleo@gmail.com>
2019-03-21 12:24:10 -0400

    Updates

---
+++ kubernetes_config_map.tf
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "kubernetes_config_map" "elasticsearch_config" {
+  metadata {
+    name = "${var.release_name}-configmap"
+
+    labels {
+      "app.kubernetes.io/name"      = "${var.release_name}"
+      "app.kubernetes.io/component" = "elasticsearch-server"
+    }
+  }
+
+  data {
+    elasticsearch.yml = <<EOF
+    cluster.name: "$${CLUSTER_NAME}"
+    node.name: "$${NODE_NAME}"
+
+    path.data: /usr/share/elasticsearch/data
+    path.repo: ["$${BACKUP_REPO_PATH}"]
+
+    network.host: 0.0.0.0
+
+    discovery.zen.minimum_master_nodes: 2
+    discovery.zen.ping.unicast.hosts: $${DISCOVERY_SERVICE}
+    EOF
+
+    log4j2.properties = <<EOF
+    status = error
+
+    appender.console.type = Console
+    appender.console.name = console
+    appender.console.layout.type = PatternLayout
+    appender.console.layout.pattern = [%d{ISO8601}][%-5p][%-25c{1.}] %marker%m%n
+
+    rootLogger.level = info
+    rootLogger.appenderRef.console.ref = console
+    EOF
+  }
+
+  depends_on = ["null_resource.get_cluster_credentials"]
+}
---
+++ scripts/install_terraform.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip -O terraform_install.zip && \
+unzip -o terraform_install.zip && \
+sudo install terraform /usr/local/bin/ && \
+echo "Successfully installed $(terraform --version)";
