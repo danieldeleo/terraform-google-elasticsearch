7746290b5f8720e2e22ba5924e15e860765a8d60
danielrdeleo <danielrdeleo@gmail.com>
2019-03-23 16:06:50 -0400

    Updates

--- module/main.tf
+++ module/main.tf
@@ -18,17 +18,23 @@ provider "google-beta" {
   region = "${var.region}"
 }
 
-resource "google_compute_subnetwork" "elasticsearch_subnetwork" {
+data "google_compute_subnetwork" "elasticsearch_subnetwork" {
   name                     = "${var.subnetwork}"
-  ip_cidr_range            = "${var.subnetwork_ip_cidr_range}"
-  region                   = "${var.region}"
-  private_ip_google_access = "true"
-  enable_flow_logs         = "true"
-  network                  = "${var.network}"
   project                  = "${var.project_id}"
-
-  secondary_ip_range = "${var.secondary_ranges}"
+  region                   = "${var.region}"
 }
+
+//resource "google_compute_subnetwork" "elasticsearch_subnetwork" {
+//  name                     = "${var.subnetwork}"
+//  ip_cidr_range            = "${var.subnetwork_ip_cidr_range}"
+//  region                   = "${var.region}"
+//  private_ip_google_access = "true"
+//  enable_flow_logs         = "true"
+//  network                  = "${var.network}"
+//  project                  = "${var.project_id}"
+//
+//  secondary_ip_range = "${var.secondary_ranges}"
+//}
 
 module "elasticsearch_cluster" {
   source                     = "github.com/terraform-google-modules/terraform-google-kubernetes-engine/modules/private-cluster/"
@@ -38,9 +44,9 @@ module "elasticsearch_cluster" {
   region                     = "${var.region}"
   zones                      = "${var.zones}"
   network                    = "${var.network}"
-  subnetwork                 = "${google_compute_subnetwork.elasticsearch_subnetwork.name}"
-  ip_range_pods              = "${lookup(google_compute_subnetwork.elasticsearch_subnetwork.secondary_ip_range[0], "range_name")}"
-  ip_range_services          = "${lookup(google_compute_subnetwork.elasticsearch_subnetwork.secondary_ip_range[1], "range_name")}"
+  subnetwork                 = "${data.google_compute_subnetwork.elasticsearch_subnetwork.name}"
+  ip_range_pods              = "${lookup(data.google_compute_subnetwork.elasticsearch_subnetwork.secondary_ip_range[0], "range_name")}"
+  ip_range_services          = "${lookup(data.google_compute_subnetwork.elasticsearch_subnetwork.secondary_ip_range[1], "range_name")}"
   service_account            = "${var.compute_engine_service_account}"
   enable_private_endpoint    = false
   enable_private_nodes       = true
@@ -50,7 +56,10 @@ module "elasticsearch_cluster" {
   master_ipv4_cidr_block = "172.16.0.0/28"
 
   master_authorized_networks_config = [{
-    cidr_blocks = "${var.master_authorized_cidr_blocks}"
+    cidr_blocks =  [{
+      cidr_block   = "${data.google_compute_subnetwork.elasticsearch_subnetwork.ip_cidr_range}"
+      display_name = "${data.google_compute_subnetwork.elasticsearch_subnetwork.name}"
+    }]
   }]
 
   remove_default_node_pool = "true"
