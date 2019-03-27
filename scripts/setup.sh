45873eecbf076308b6af7f6814fbfb36a4d274fc
danielrdeleo <danielrdeleo@gmail.com>
2019-03-27 14:38:35 -0400

    Updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install git jq unzip kubectl -y
 git clone https://github.com/danieldeleo/terraform-google-elasticsearch.git
-cd terraform-google-elasticsearch/module && ./scripts/install_terraform.sh
+cd terraform-google-elasticsearch && ./scripts/install_terraform.sh
 terraform init
 terraform apply -auto-approve
 
