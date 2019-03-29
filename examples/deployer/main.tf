d551cb67c6ffe12bb83eca1080b7e60711249fb4
danielrdeleo <danielrdeleo@gmail.com>
2019-03-29 19:02:05 -0400

    Updates

--- examples/deployer/main.tf
+++ examples/deployer/main.tf
@@ -20,22 +20,10 @@ module "instance_template" {
   source          = "github.com/terraform-google-modules/terraform-google-vm/modules/instance_template"
   subnetwork      = "${var.subnetwork}"
   service_account = "${var.compute_engine_service_account}}"
-  startup_script  = "${var.startup_script}"
 }
-//
-//module "compute_instance" {
-//  source            = "github.com/terraform-google-modules/terraform-google-vm/modules/compute_instance"
-//  subnetwork        = "${var.subnetwork}"
-//  num_instances     = "1"
-//  hostname          = "instance-simple"
-//  instance_template = "${module.instance_template.self_link}"
-//}
 
 resource "google_compute_instance_from_template" "example" {
   name = "elasticsearch_deployer"
-  network_interface {
-    subnetwork = "${google_compute_subnetwork.elasticsearch_subnetwork.name}"
-  }
 
   source_instance_template = "${module.instance_template.self_link}"
   metadata {
