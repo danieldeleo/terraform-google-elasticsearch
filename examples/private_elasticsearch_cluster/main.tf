ff54e7ea976124771ccfaaa93331486d68c7eebd
danielrdeleo <danielrdeleo@gmail.com>
2019-04-01 17:00:51 -0400

    Updates

--- examples/private_elasticsearch_cluster/main.tf
+++ examples/private_elasticsearch_cluster/main.tf
@@ -24,24 +24,6 @@ provider "kubernetes" {
   token = "${data.google_client_config.default.access_token}"
 }
 
-//resource "google_compute_subnetwork" "elasticsearch_subnetwork" {
-//  name                     = "${var.subnetwork}"
-//  ip_cidr_range            = "${var.subnetwork_ip_cidr_range}"
-//  region                   = "${var.region}"
-//  private_ip_google_access = "true"
-//  enable_flow_logs         = "true"
-//  network                  = "${var.network}"
-//  project                  = "${var.project_id}"
-//
-//  secondary_ip_range = "${var.secondary_ranges}"
-//}
-
-//data "google_compute_subnetwork" "elasticsearch_subnetwork" {
-//  project = "${var.project_id}"
-//  region = "${var.region}"
-//  name = "${google_compute_subnetwork.elasticsearch_subnetwork.name}"
-//}
-
 data "google_compute_subnetwork" "elasticsearch_subnetwork" {
   project = "${var.project_id}"
   region = "${var.region}"
