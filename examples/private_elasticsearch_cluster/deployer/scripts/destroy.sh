e31a74a6d6f92cbd6d79c9bc9018e4bed1fbb1b1
danielrdeleo <danielrdeleo@gmail.com>
2019-04-05 17:38:47 -0400

    Updates

--- examples/private_elasticsearch_cluster/deployer/scripts/destroy.sh
+++ examples/private_elasticsearch_cluster/deployer/scripts/destroy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-cd terraform-google-elasticsearch/examples/private_elasticsearch_cluster
+cd /terraform-google-elasticsearch/examples/private_elasticsearch_cluster
 terraform destroy -auto-approve -var project_id=$(gcloud compute project-info describe --format="value(name)")
+cd /terraform-google-elasticsearch/examples/private_elasticsearch_cluster/
